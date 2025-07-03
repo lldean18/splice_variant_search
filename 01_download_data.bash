9c5efb1c7674917283ad81b90d1642d25a8d7fc6
Laura Dean <lldean18@gmail.com>
2025-07-03 13:46:39 +0100

    needed to switch the order of tmux activation then srun command because it doesn't work the other way arouund

--- 01_download_data.bash
+++ 01_download_data.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# start an interactive compute job
-srun --partition defq --cpus-per-task 1 --mem 30g --time 48:00:00 --pty bash
 
 # create a tmux session
 source $HOME/.bash_profile
 conda activate tmux
 tmux new -s download
 
+
+# start an interactive compute job
+srun --partition defq --cpus-per-task 1 --mem 70g --time 160:00:00 --pty bash
+
+
 # start the data pull command
-rsync -rvh --progress smaug:/data/test_data/simon_RNA/bams_from_hpc /gpfs01/home/mbzlld/data/splice_variant_search/Simons_data/
+rsync -rvh --progress smaug:/data/test_data/simon_RNA/bams_from_hpc /gpfs01/home/mbzlld/data/splice_variant_search/
 
 # cntrl+b, then d to detatch from the tmux window while the command is running
 
