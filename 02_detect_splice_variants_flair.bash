032ab69d41ce8d56faae6511e1df43760a4bad98
Laura Dean <lldean18@gmail.com>
2025-07-24 10:48:20 +0100

    first try at running the first few steps of flair

--- 02_detect_splice_variants_flair.bash
+++ 02_detect_splice_variants_flair.bash
@@ -8,8 +8,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=30g
-#SBATCH --time=48:00:00
+#SBATCH --mem=200g
+#SBATCH --time=60:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
@@ -18,8 +18,9 @@
 wkdir=/gpfs01/home/mbzlld/data/splice_variant_search
 reference=$wkdir/GCF_000001405.40_GRCh38.p14_genomic.fna
 annotation=$wkdir/GCF_000001405.40_GRCh38.p14_genomic.gtf
-bam=$wkdir/bams_from_hpc/GIN44_DMSO_merged.bam
-fastqs=
+bam=$wkdir/bams_from_hpc/SF188_DMSO_merged.bam
+fastqs=$wkdir/ds1239_SF188_DMSO_RNA/20241107_1347_P2S-01121-B_PBA03961_2b1a6105/fastq_pass/*
+fastqs2=/gpfs01/home/mbzlld/data/splice_variant_search/ds1239_SF188_DMSO_RNA/20241110_1301_P2S-01121-B_PBA03961_ac423a17/fastq_pass/*
 
 cd $wkdir
 
@@ -45,16 +46,18 @@ flair collapse \
 	--genome $reference \
 	--gtf $annotation \
 	--query ${bam%.*}.bed12 \
-	--reads $fastqs \
+	--reads $fastqs $fastqs2 \
 	--generate_map \
 	--check_splice \
 	--stringent \
 	--annotation_reliant generate
 
+# WILL CARRY ON FROM HERE IN A SEPARATE RUN IF THE FIRST BIT WORKS!!
+
 # flair quantify - 
-flair quantify \
-	--isoforms ? \
-	--reads_manifest ?
+#flair quantify \
+#	--isoforms ? \
+#	--reads_manifest ?
 
 # flair diffsplice - 
 
