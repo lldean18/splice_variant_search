81125c05c286dbc65db005289af8744d0f305408
Laura Dean <lldean18@gmail.com>
2025-11-06 11:12:45 +0000

    adding script to process the output

---
+++ 04_process_flair_isoform_counts.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Laura Dean
+# 3/11/25
+# script written for running on the UoN HPC Ada
+
+# Script to process FLAIR quantify output to determine the relative expression
+# of the different splice variants of a gene in this cancer RNAseq dataset
+
+#SBATCH --job-name=flair_process
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5g
+#SBATCH --time=00:30:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+
+# set variables
+wkdir=/gpfs01/home/mbzlld/data/splice_variant_search
+reference=$wkdir/GCF_000001405.40_GRCh38.p14_genomic.fna
+annotation=$wkdir/GCF_000001405.40_GRCh38.p14_genomic.gtf
+gene_of_interest=KCNMA1
+
+cd $wkdir
+
+
+# sum across replicates of the same sample (which in this case is all of them)
+awk '{for(i=2;i<=NF;i++) a[$1]+=$i} END{for(k in a) print k,a[k]}' flair.quantify.counts.tsv > flair.quantify.counts.summed.tsv
+
+# extract only the splice variants for the gene of interest
+grep -i $gene_of_interest flair.quantify.counts.summed.tsv > ${gene_of_interest}.flair.quantify.counts.summed.tsv
+
+
