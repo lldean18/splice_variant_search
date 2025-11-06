6efbb239c4649de3503a35bad75eda95aec72d91
Laura Dean <lldean18@gmail.com>
2025-11-06 11:14:29 +0000

    removing bits of the script that arent needed

--- 04_process_flair_isoform_counts.bash
+++ 04_process_flair_isoform_counts.bash
@@ -1,20 +1,10 @@
 #!/bin/bash
 # Laura Dean
-# 3/11/25
+# 6/11/25
 # script written for running on the UoN HPC Ada
 
 # Script to process FLAIR quantify output to determine the relative expression
 # of the different splice variants of a gene in this cancer RNAseq dataset
-
-#SBATCH --job-name=flair_process
-#SBATCH --partition=defq
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=5g
-#SBATCH --time=00:30:00
-#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
-
 
 
 # set variables
