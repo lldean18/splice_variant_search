5c7a4815e35a2cfcff6d5f86fdfd23c8d2bbe536
Laura Dean <lldean18@gmail.com>
2025-11-07 10:16:18 +0000

    updating data download script to download fastqs rather than bams bc flair needed the fastqs in the end

--- 01_download_data.bash
+++ 01_download_data.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # code to download the bams from Smaug to Ada
 # needed the fastq files in the end for flair rather than the bams
+# so added the code to pull the fastqs from balrog (mounted from elrond) to Ada
 
 # create a tmux session
 source $HOME/.bash_profile
@@ -12,8 +13,14 @@ tmux new -s download
 srun --partition defq --cpus-per-task 1 --mem 70g --time 160:00:00 --pty bash
 
 
-# start the data pull command
-rsync -rvh --progress smaug:/data/test_data/simon_RNA/bams_from_hpc /gpfs01/home/mbzlld/data/splice_variant_search/
+## start the data pull command for the bams
+#rsync -rvh --progress smaug:/data/test_data/simon_RNA/bams_from_hpc /gpfs01/home/mbzlld/data/splice_variant_search/
+
+# start the data pull command for the fastqs
+# removed the real username and IP address for security
+rsync -rvhc --progress balrogusername@balrigipaddress:/share/NFSv=4/deepseq/IntraopBackup/ds1239_SF188_DMSO_RNA ./
+
+
 
 # cntrl+b, then d to detatch from the tmux window while the command is running
 
