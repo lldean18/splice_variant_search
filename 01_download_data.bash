0332f1dfba72499222ea42bcee4f91daa01f5eea
Laura Dean <lldean18@gmail.com>
2025-11-07 10:44:23 +0000

    adding information about new location of the raw data

--- 01_download_data.bash
+++ 01_download_data.bash
@@ -19,7 +19,9 @@ srun --partition defq --cpus-per-task 1 --mem 70g --time 160:00:00 --pty bash
 # start the data pull command for the fastqs
 # removed the real username and IP address for security
 rsync -rvhc --progress balrogusername@balrogipaddress:/share/NFSv=4/deepseq/IntraopBackup/ds1239_SF188_DMSO_RNA ./
-
+# I think Simon has changed the organisation of the directory I copied from now.
+# files are now in:
+# /share/NFSv=4/deepseq/IntraopBackup/ds1239/cell_lines/
 
 
 # cntrl+b, then d to detatch from the tmux window while the command is running
