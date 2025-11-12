7ceb82e27fce694430cbee59691e1e177822861d
Laura Dean <lldean18@gmail.com>
2025-11-12 11:19:55 +0000

    uploading current progress with script

--- 05_blast_primers_against_db_of_variants.bash
+++ 05_blast_primers_against_db_of_variants.bash
@@ -1,12 +1,24 @@
 
+######## SET UP ENVIRONMENT ###########
+
 # load env settings
 source $HOME/.bash_profile
 
 # activate conda env sith blast install
 conda activate phylogeny
 
+# move to working directory
+cd /gpfs01/home/mbzlld/data/splice_variant_search
+
+
+
+
+
+########## PERFORM TASKS #############
 
 # make a blast database from a fasta file of splice variants
-makeblastdb -dbtype prot -in SubjectFile.fasta -parse_seqids -out blast_db
+makeblastdb -dbtype nucl -in KCNMA1.variant.sequences.fa -parse_seqids -out KCNMA1_variant_blast_db
 
+# blast the query primer sequences against the database
+blastp -query mouse_STREX_primers.fasta -db KCNMA1_variant_blast_db -outfmt '6 sseqid' -evalue 1e-5 -out KCNMA1_variant_blast_output.txt
 
